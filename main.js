abe45ad105ae44360270cf78d1301aec6e1e0a0b
mack66hm <mack66hm@gmail.com>
2020-01-29 18:25:00 -0500

    add click option and appear in total

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let answer = document.querySelector('.totalBar')
+let number = document.querySelector('.seven')
+console.log(number)
+number.addEventListener('click', function() {
+    answer.innerText = '7'
+})
+
+
