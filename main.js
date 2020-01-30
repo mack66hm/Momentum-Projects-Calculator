eb48e13342fe23402847b5e1330f9633db8c4aa7
mack66hm <mack66hm@gmail.com>
2020-01-29 19:04:15 -0500

    got buttons to click

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 
 let answer = document.querySelector('.totalBar')
-let number = document.querySelector('.seven')
-console.log(number)
-number.addEventListener('click', function() {
-    answer.innerText = '7'
+let buttons = document.querySelectorAll('.row')
+console.log(buttons)
+
+for (let button of buttons){
+    button.addEventListener('click', function() {
+ let newEl = document.createElement('p')
+ let text = document.createTextNode(button.innerText)
+  newEl.appendChild(text)  
+ answer.appendChild(newEl)
 })
+}
 
 
